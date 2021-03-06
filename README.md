f04a0b7df3be10df7b894a0c8f7c860dbdf6abf4
Tim Ratigan <tratigan@alumni.princeton.edu>
2021-03-05 20:39:21 -0600

    Added more fully featured version of `texas.py`

--- README.md
+++ README.md
@@ -9,11 +9,7 @@ To copy this package, run:
 
 ## Texas
 
-### On Windows
-
-Download the file `dist/texas.exe`, it's an executable file that can run directly on your machine. If you want to restrict the cities open your command line (by searching cmd) and type the path to `texas.exe` followed by the cities you want, like I explained in the Python version below.
-
-### Using Python
+### Using Python (simplified)
 
 Run the following in the command line:
 
@@ -26,3 +22,39 @@ Where `cities` are the cities you want to restrict your search to. If none are p
 Once it starts running, it will ping HEB periodically until an appointment in one of those cities shows up, and then open it in your web browser. You still have to be quick from there!
 
 The loop will stop running once it finds a page to open, so if you don't get the appointment on the first try just run it again!
+
+### Using Python (fancy)
+
+If you feel comfortable installing a few Python packages, the required packages are in `requirements.txt`
+
+Run `python texas_fancy.py --help` for usage:
+
+```
+usage: texas_fancy.py [-h] [-c CITIES [CITIES ...]] [-H HOME] [-d DISTANCE] [-Z ZIPCODES [ZIPCODES ...]]
+
+Program to ping HEB for vaccine appointments in your area
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CITIES [CITIES ...], --cities CITIES [CITIES ...]
+                        Cities to restrict the search to
+  -H HOME, --home HOME  Home location: can be in the form of a zipcode, address, latitude/longitude, city, etc. (requires distance)
+  -d DISTANCE, --distance DISTANCE
+                        Maximum distance (in miles) from home (requires home)
+  -Z ZIPCODES [ZIPCODES ...], --zipcodes ZIPCODES [ZIPCODES ...]
+                        Zipcodes to restrict the search to
+
+```
+
+Examples:
+
+ - `python texas_fancy.py -c "San Antonio" Jourdanton Pleasanton Leming Poteet` would do the same as the simple example
+ - `python texas_fancy.py -H "San Antonio, TX" -d 50` would look for appointments 50 miles from San Antonio
+ - `python texas_fancy.py -H 78023 -d 30` would look for appointments 30 miles from zip code 78023
+ - `python texas_fancy.py -z 76028 75165` would look for appointments only in the zip codes 76028 and 75165
+
+### On Windows
+
+_Note: You probably shouldn't be trusting me with random executables. The Python version is recommended if you can work out how to run it._
+
+Under the `dist` folder there's a `.exe` file corresponding to each of the Python files. Both of them can be run from the command line out of the box on a Windows machine using the same commands. For example, if you wanted to run the first `texas_fancy.py` example, you would open your command prompt (search for "cmd" on your computer) and then run `texas_fancy.exe -c "San Antonio" Jourdanton Pleasanton Leming Poteet` after downloading the file to your home directory.
---
+++ texas_fancy.py
@@ -0,0 +1,76 @@
+from time import sleep
+
+import argparse
+import requests
+import sys
+from tqdm import tqdm
+import webbrowser
+import urllib.request
+
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+
+store_name_to_distance = {}
+
+
+def open_appointments(namespace, geolocator):
+    locations = requests.get('https://heb-ecom-covid-vaccine.hebdigital-prd.com/vaccine_locations.json').json()[
+        'locations']
+    success = False
+    for location in locations:
+        if namespace.cities is not None and location['city'].lower() not in namespace.cities:
+            continue
+        if namespace.zipcodes is not None and location['zip'] not in namespace.zipcodes:
+            continue
+        distance = None
+        if namespace.distance is not None:
+            if location['name'] in store_name_to_distance:
+                distance = store_name_to_distance[location['name']]
+            else:
+                latlong = (location['latitude'], location['longitude'])
+                if any(l is None for l in latlong):
+                    geoloc = geolocator.geocode(', '.join(location[key] for key in ['street', 'city', 'state', 'zip']))
+                    if geoloc is None:
+                        geoloc = geolocator.geocode(location['zip'])
+                    latlong = (geoloc.latitude, geoloc.longitude)
+                distance = geodesic(ns.latlong, latlong)
+                store_name_to_distance[location['name']] = distance
+            if distance.miles > namespace.distance:
+                continue
+        if location['openTimeslots'] > 0:
+            contents = urllib.request.urlopen(location['url']).read().decode('utf-8')
+            if 'Appointments are no longer available for this location' not in contents:
+                webbrowser.open(location['url'])
+                print('\n'.join(f'{k}={v}' for k, v in location.items() if k not in ['url', 'slotDetails'] and v is not None))
+                if distance is not None:
+                    print(f'Distance from home: {distance.miles} miles')
+                success = True
+    return success
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Program to ping HEB for vaccine appointments in your area")
+    parser.add_argument('-c', '--cities', nargs='+', help='Cities to restrict the search to')
+    parser.add_argument('-H', '--home',
+                        help='Home location: can be in the form of a zipcode, address, latitude/longitude, city, etc. (requires distance)')
+    parser.add_argument('-d', '--distance', type=float,
+                        help='Maximum distance (in miles) from home (requires home)')
+    parser.add_argument('-Z', '--zipcodes', nargs='+', help='Zipcodes to restrict the search to')
+
+    ns = parser.parse_args(sys.argv[1:])
+
+    assert (ns.distance is None) == (ns.home is None), 'Home location and distance should be supplied together'
+
+    geolocator = Nominatim(user_agent='vaccine_appointments')
+
+    if ns.cities:
+        ns.cities = {city.lower() for city in ns.cities}
+    if ns.distance:
+        home = geolocator.geocode(ns.home)
+        ns.latlong = (home.latitude, home.longitude)
+        print(f'Looking for appointments {ns.distance} miles from {home}')
+    with tqdm() as pbar:
+        while not open_appointments(ns, geolocator):
+            sleep(1)
+            pbar.update(1)
